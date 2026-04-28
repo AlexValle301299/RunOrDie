5c5a9b093d06d89a5f123a4c64cef7394990b579
AlexValle301299 <a.valle301299@gmail.com>
2026-04-28 13:19:47 +0200

    main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Punto de entrada del juego (Día 1: Entry Point)
+// Se ejecuta cuando el DOM está completamente cargado
+window.addEventListener('load', () => {
+  const game = new Game();
+  game.start();
+});
